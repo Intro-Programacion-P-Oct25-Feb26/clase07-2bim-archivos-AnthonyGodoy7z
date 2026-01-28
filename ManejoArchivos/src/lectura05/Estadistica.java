15b232672eb592e3e6522597b4dbc3c64c2edd74
Anthony <tonyxiz10@gmail.com>
2026-01-28 12:48:51 -0500

    commit

---
+++ ManejoArchivos/src/lectura05/Estadistica.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lectura05;
+
+/**
+ *
+ * @author UTPL
+ */
+public class Estadistica {
+
+    public static void reporte(String args[]) {
+
+    }
+}
